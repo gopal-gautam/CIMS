569d23a62dcab73bb9af19d47d35d3c88c8aae1f
shirish7151 <nirish123>
2015-11-20 12:19:04 +0545

    New GroupUsers Model

---
+++ CMISProject/Models/GroupsUsers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CMISProject.Models
+{
+    public class GroupsUsers
+    {
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+
+        [Required]
+        [ForeignKey("GroupId")]
+        public Group Group { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
