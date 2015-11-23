a2dd21a39377c2986b3ec943e0be7cc1ba01a021
shirish7151 <nirish123>
2015-11-23 21:44:25 +0545

    ViewMessageViewModel is added to provide viewing of message

--- CMISProject/ViewModels/MessageViewModels/MessageViewModel.cs
+++ CMISProject/ViewModels/MessageViewModels/MessageViewModel.cs
@@ -7,7 +7,8 @@ using System.Web;
 
 namespace CMISProject.ViewModels.MessageViewModels
 {
-    public class SendMessageViewModel
+
+    private class MessageViewModel
     {
         [Required]
         [Display(Name = "MessageType")]
@@ -21,44 +22,46 @@ namespace CMISProject.ViewModels.MessageViewModels
         [UIHint("Message")]
         public string Msg { get; set; }
 
-        [DataType(DataType.Upload)]
-        [UIHint("Upload File")]
-        public HttpPostedFileBase Attachment { get; set; }
+        
 
-        public React React { get; set; }
+        //public React React { get; set; }
 
         [Required]
         [UIHint("Message Mode")]
         public MessageMode Mode { get; set; }
 
-        //public GroupMessage groupMessage { get; set; }
-        public UserMessage userMessage { get; set; }
 
     }
-    public class GroupMessageViewModel
+    
+    public class SendMessageViewModel : MessageViewModel
     {
-        [Required]
-        [Display(Name = "MessageType")]
-        [UIHint("Message Type")]
-        public MessageType MessageType { get; set; }
-
-        [Required]
-        [Display(Name = "Message")]
-        [UIHint("Enter Message")]
-        [DataType(DataType.MultilineText)]
-        [UIHint("Message")]
-        public string Msg { get; set; }
 
         [DataType(DataType.Upload)]
         [UIHint("Upload File")]
         public HttpPostedFileBase Attachment { get; set; }
 
-        public React React { get; set; }
+                //public GroupMessage groupMessage { get; set; }
+        public UserMessage userMessage { get; set; }
 
-        [Required]
-        [UIHint("Message Mode")]
-        public MessageMode Mode { get; set; }
+        //public MessageReaction MessageReaction { get; set; }
+
+    }
+    public class GroupMessageViewModel : MessageViewModel
+    {
+        [DataType(DataType.Upload)]
+        [UIHint("Upload File")]
+        public HttpPostedFileBase Attachment { get; set; }
 
         public GroupMessage groupMessage { get; set; }
+
+        //public MessageReaction MessageReaction { get; set; }
+    }
+
+    public class ViewMessageViewModel : MessageViewModel
+    {
+        // public MessageReaction MessageReaction { get; set; }
+        public int LikeCounts { get; set; }
+        public int DislikeCounts { get; set; }
+        public Resource Attachment { get; set; }
     }
 }
