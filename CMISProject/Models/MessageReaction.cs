180d681df351c881e6a30b2360ac954d5ffdf7b6
shirish7151 <nirish123>
2015-11-23 21:43:25 +0545

    MessageReaction Model is added

---
+++ CMISProject/Models/MessageReaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CMISProject.Models
+{
+
+    public enum React
+    {
+        Like, Dislike,
+    }
+    public class MessageReaction
+    {
+        public int Id { get; set; }
+
+        public int MessageId { get; set; }
+
+        [ForeignKey("MessageId")]
+        public Message Message { get; set; }
+
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        public React React { get; set; }
+    }
+}
