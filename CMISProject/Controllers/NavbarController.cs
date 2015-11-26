ea0a9c694ac9ae840c7483f6c83795785c3f5408
shirish7151 <nirish123>
2015-11-26 15:05:00 +0545

    Dynamic Navbar additions

--- CMISProject/Controllers/NavbarController.cs
+++ CMISProject/Controllers/NavbarController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CMISProject.Models;
 
 namespace sb_admin_2.Web1.Controllers
 {
@@ -14,7 +15,22 @@ namespace sb_admin_2.Web1.Controllers
         public ActionResult Index()
         {
             var data = new Data();
-            return PartialView("_Navbar", data.navbarItems().ToList());
+            var navBarItems = data.navbarItems().ToList();
+            if(User.Identity.IsAuthenticated)
+            {
+                int curUserId = (int) HttpContext.Session["UserId"];
+                var db = new CMISProject.DAL.CIMSEntities();
+                int totalNavItem = data.navbarItems().Count();
+                var groupsUsers = db.Users.Join(db.GroupUserRelations.Where(s => s.UserId == curUserId), o => o.UserId, i => i.UserId, (c, o) => new { c, o });
+                foreach( var groupUser in groupsUsers)
+                {
+                    Group group = groupUser.o.Group;
+                    navBarItems.Add(new Navbar { Id = totalNavItem+1, nameOption = group.GroupName, controller = "Group", action = "Create", status = true, isParent = false, parentId = 19 });
+
+                }
+
+            }
+            return PartialView("_Navbar", navBarItems);
         }
     }
 }
